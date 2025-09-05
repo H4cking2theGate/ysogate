745629f3de0ebff9c6ae824afdaa1ad9664f3e93
H4cking2theGate <h4cking2thegate@gmail.com>
2025-09-06 02:20:46 +0800

    del temp/Foo

--- src/main/java/temp/Foo.java
+++
@@ -1,9 +0,0 @@
-package temp;
-
-import java.io.Serializable;
-
-// required to make TemplatesImpl happy
-public class Foo implements Serializable {
-
-    private static final long serialVersionUID = 8207363842866235160L;
-}
